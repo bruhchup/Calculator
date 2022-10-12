ae4ba7f3c56b065d75327a500954e3a4e1385bf3
bruhchup <115509602+bruhchup@users.noreply.github.com>
2022-10-12 00:32:18 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+print("Hayden Instruments")
+print("Select an operation")	
+print("1:+")
+print("2:-")
+print("3:*")
+print("4:/")
+print("5:y√x ")
+print("5:x**y ")
+
+oper = input()
+if oper == "1":		
+	num1 = input("Enter first number")
+	num2 = input("Enter second number")
+	print (+ num1 + num2)
+
+input ()
