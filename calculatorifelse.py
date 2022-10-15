d65c8149bdd386d617524f2ea52e67b7819d4717
bruhchup <115509602+bruhchup@users.noreply.github.com>
2022-10-14 20:48:36 -0400

    Add files via upload

--- calculatorifelse.py
+++ calculatorifelse.py
@@ -1,9 +1,9 @@
 import faulthandler
 
-
+print("HTech©")
 repeat = "y"
 while repeat == "y":
-
+	
 	print("select an operation")
 	print("1.+")
 	print("2.-")
@@ -12,6 +12,7 @@ while repeat == "y":
 	print("5.x**y")
 	print("6.√x")
 	print("7.y√x")
+	print("8.exit program")
 
 	while True:
 		try:
@@ -22,11 +23,11 @@ while repeat == "y":
 			print("select an operation")
 		continue
 	
-	if oper < 1 or oper > 7 or oper == "":
+	if oper < 1 or oper > 8 or oper == "":
 		print("invalid operation")
 		continue	
 
-	while True:
+	while oper != 8:
 		try:
 			num1 = float(input("enter first number"))
 			print(num1)
@@ -34,7 +35,7 @@ while repeat == "y":
 		except ValueError:
 			print("invalid input")	
 		
-	while oper != 6:
+	while oper != 6 and oper != 8:
 		try:
 			num2 = float(input("enter second number"))
 			print(num2)
@@ -60,9 +61,21 @@ while repeat == "y":
 			print("OverFlowError: result too large")
 	elif oper == 6:
 			comp = (num1 ** (1/2))	
-			print ((comp.imag),"j")
+			if num1 < 0:
+				print ((comp.imag),"j")
+			elif num1 > 0:
+				print (comp)
 	elif oper == 7:
 			print (num1 ** (1/num2))
+	elif oper == 8:
+			aresure = input("are you sure? y/n")
+			if aresure == "y":
+				exit()
+			if aresure == "n":
+				break
+			else:
+				print (aresure,"is an invalid input")
+			continue
 
 	while repeat := input("perform another operation? y/n"):
 		if repeat == "n":
